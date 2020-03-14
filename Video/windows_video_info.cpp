2a9ae295394a4f3ff52502552efb3c7c80c3a412
wk1219 <sjms1215@naver.com>
2020-03-14 23:01:15 +0900

    update windows video info

--- Video/windows_video_info.cpp
+++ Video/windows_video_info.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 #include <windows.h>
 #include <locale>
+#include <string.h>
+#include <opencv2/opencv.hpp>
+
 #pragma warning(disable: 4996)
 using namespace std;
+using namespace cv;
 
-void FileList(char *path) {
+void FileList(char *path, char *ppath) {
 	WIN32_FIND_DATA wfd;
 	HANDLE hSrch;
 	BOOL bResult = TRUE;
@@ -13,6 +17,7 @@ void FileList(char *path) {
 	char newpath[MAX_PATH];
 	char file[MAX_PATH];
 	int fileSize = -1;
+	
 	cout << "Search Path = " << path << endl;
 	hSrch = FindFirstFile(path, &wfd);
 	if (hSrch == INVALID_HANDLE_VALUE) {
@@ -25,17 +30,31 @@ void FileList(char *path) {
 				cout << newpath << drive << dir << wfd.cFileName << endl;
 
 				cout << "File Size : " << fileSize << endl;
-				FileList(newpath);
+				FileList(newpath, ppath);
 			}
 		}
 		else {
 			cout << drive << dir << wfd.cFileName << endl;
+			strcat(ppath, wfd.cFileName);
+			VideoCapture cap(ppath);
+			int width = cvRound(cap.get(CAP_PROP_FRAME_WIDTH));
+			int height = cvRound(cap.get(CAP_PROP_FRAME_HEIGHT));
+			double fps = cap.get(CAP_PROP_FPS);
+			int fcount = cvRound(cap.get(CAP_PROP_FRAME_COUNT));
 			fileSize = (((int)wfd.nFileSizeHigh) << 32) + wfd.nFileSizeLow;
+		
 			cout << "File Name : " << wfd.cFileName << endl;
 			cout << "File Size : " << fileSize << " Byte" << endl;
-			cout << "Length : " << endl;
-			//cout << "Make time : " << wfd.ftCreationTime << endl;
-			cout << "Resolution : " << endl;
+			// cout << "Length : " << wfd.dwFileAttributes << endl;
+			// cout << "Make time : " << wfd.ftCreationTime << endl;
+			cout << "Resolution : " << width << "X" << height << endl;
+			cout << "File Path : " << ppath << endl;
+
+			cout << "FPS : " << fps << endl;
+			cout << "Frame Count : " << fcount << endl;
+
+
+			// cout << GetFileAttributes(wfd.cFileName) << endl;
 		}
 		bResult = FindNextFile(hSrch, &wfd);
 	}
@@ -44,12 +63,13 @@ void FileList(char *path) {
 
 void main()
 {
-	std::locale::global(std::locale("ko_KR.UTF-8"));
+	//std::locale::global(std::locale("ko_KR.UTF-8"));
 	// char Path[MAX_PATH];
 	// GetWindowsDirectory(Path, MAX_PATH);   // System Directory (Windows)
-	char Path[100] = "C:\\Users\\sjms1\\Desktop\\video";
+	char Path[100] = "Path";
+	char path[100] = "ppath";
 
 	strcat(Path, "\\*.*");
-	FileList(Path);
+	FileList(Path, path);
 	// cout << "MAX_PATH : "<< Path << endl;
 }
