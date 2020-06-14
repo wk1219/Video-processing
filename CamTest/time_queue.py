b9ed21e8aa590f593b52a5eec055a13ad4fad9bf
wk1219 <sjms1215@naver.com>
2020-06-15 01:29:20 +0900

    Update time_queue.py

--- CamTest/time_queue.py
+++ CamTest/time_queue.py
@@ -4,19 +4,19 @@ def create_time():
     now = datetime.datetime.today().strftime("%y%m%d_%H%M%S")
     return now
 
-impt_time = []
-
+impt = []
 while True:
     t = create_time()
+    file_name = t + '.mp4'
     sec = int(t[11:13])
-    if sec % 2 == 1:
-        impt_time.append(t)
-        print(sec)
-    elif sec == 20:
+    impt.append((file_name, sec))
+    time_queue = sorted(set(impt), reverse=False)
+    if sec == 40:
         break
 
-time_queue = sorted(list(set(impt_time)), reverse=False)
-queue_size = len(time_queue)
+keys = list(dict(time_queue).keys())        # Video file name list
+values = list(dict(time_queue).values())    # Impact time (sec) list
 
-for i in range(0, queue_size):
-    print("%s" % time_queue[i])
+for i in range(0, len(time_queue)):
+    print("[%d] file_name : %s, impt_time : %d" % (i, keys[i], values[i]))
+    # print("[%d] file_name : %s" % (i, time_queue[i]))
