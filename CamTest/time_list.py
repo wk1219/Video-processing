8387ef8ffd30a95691d3c2f592aca2957a3554bb
wk1219 <sjms1215@naver.com>
2020-06-16 16:21:43 +0900

    Update time_list.py

--- CamTest/time_list.py
+++ CamTest/time_list.py
@@ -4,25 +4,34 @@ def create_time():
     now = datetime.datetime.today().strftime("%y%m%d_%H%M%S")
     return now
 
-impt = []
+class ListQueue:
+    def __init__(self):
+        self.my_list = list()
+
+    def put(self, element):
+        self.my_list.append(element)
+        self.my_list = sorted(set(self.my_list), reverse=True)
+
+    def get(self):
+        return self.my_list.pop(0)
+
+    def qsize(self):
+        return len(self.my_list)
+
+
+time_queue = ListQueue()
 while True:
     t = create_time()
     file_name = t + '.mp4'
     sec = int(t[11:13])
     impt_tuple = (file_name, sec)
-    impt.append(impt_tuple)
-    time_queue = sorted(set(impt), reverse=True)
+    time_queue.put(impt_tuple)
+
     if sec == 20:
         break
 
-    time_list = list(time_queue)
+for i in range(0, time_queue.qsize()):
+    value = time_queue.get()
+    print("[%d] file_name : %s impt_time : %d" % (i, value[0], value[1]))
+print(time_queue.qsize())
 
-for i in range(0, len(time_list)):
-    print("[%d] file_name : %s impt_time : %d" % (i, time_list[i][0], time_list[i][1]))
-
-
-for i in range(0, len(time_list)):
-    data = time_list.pop()
-    print(data[0], data[1])
-
-print(len(time_list))
