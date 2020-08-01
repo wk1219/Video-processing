7b9eb68eaa92ad6a3dd26955e04bca72aab6db97
wk1219 <sjms1215@naver.com>
2020-08-01 21:32:16 +0900

    Update video_kivy.py

--- CamTest/video_kivy.py
+++ CamTest/video_kivy.py
@@ -1,15 +1,24 @@
+from tkinter import Button
+
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.screenmanager import ScreenManager
+from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
 
-class RootWidget(BoxLayout):
+class RootWidget(Screen):
     pass
 
 class WindowManager(ScreenManager):
     pass
 
 class VideoPlayerApp(App):
+    def name(self):
+        text = 'NORM_200722_230835.mp4'
+        print(text)
+        namebutton = self.root.ids.abc
+        namebutton.add_widget(Button(text='hello'))
+        return text
+
     def build(self):
         sm = ScreenManager()
         self.root = RootWidget()
