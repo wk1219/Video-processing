8bc2f414c1e482281b533971dd7c7ed0e4464b1f
wk1219 <sjms1215@naver.com>
2020-08-02 22:13:54 +0900

    Move video_kivy.py play.kv

---
+++ CamTest/UI/video_kivy.py
@@ -0,0 +1,31 @@
+from tkinter import Button
+
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.lang import Builder
+
+class RootWidget(Screen):
+    pass
+
+class WindowManager(ScreenManager):
+    pass
+
+class VideoPlayerApp(App):
+    def name(self):
+        text = 'NORM_200722_230835.mp4'
+        print(text)
+        namebutton = self.root.ids.abc
+        namebutton.add_widget(Button(text='hello'))
+        return text
+
+    def build(self):
+        sm = ScreenManager()
+        self.root = RootWidget()
+        sm.add_widget(self.root)
+        return sm
+
+ui = Builder.load_file("play.kv")
+
+if __name__ == "__main__":
+    VideoPlayerApp().run()
