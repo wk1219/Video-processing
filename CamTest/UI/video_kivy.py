c80ade5e8b10e42b70d5b3351ae9f704af7ecf9b
wk1219 <sjms1215@naver.com>
2020-08-03 22:01:23 +0900

    Update video_kivy.py

--- CamTest/UI/video_kivy.py
+++ CamTest/UI/video_kivy.py
@@ -1,26 +1,30 @@
 from tkinter import Button
-
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
+from kivy.uix.recycleboxlayout import RecycleBoxLayout
+from kivy.uix.recycleview.views import RecycleDataViewBehavior
+from kivy.properties import BooleanProperty, ListProperty
+from kivy.uix.behaviors import FocusBehavior
+from kivy.uix.recycleview.layout import LayoutSelectionBehavior
+import pymysql
 
 class RootWidget(Screen):
-    pass
+    def vidname(self):
+        vid_list = []
+        text = 'IMPT_200531_180221.mp4'
+        print(text)
+        vid_list.append(text)
+        vid_list.append('test.mp4')
+        return vid_list[1]
 
 class WindowManager(ScreenManager):
     pass
 
 class VideoPlayerApp(App):
-    def name(self):
-        text = 'NORM_200722_230835.mp4'
-        print(text)
-        # namebutton = self.root.ids.abc
-        # namebutton.add_widget(Button(text='hello'))
-        return text
-
     def build(self):
-        self.txt = self.name()
+
         sm = ScreenManager()
         self.root = RootWidget()
         sm.add_widget(self.root)
