3960afe7786d2d794c4a9deeecf9586012dfdf0b
wk1219 <sjms1215@naver.com>
2020-08-05 22:33:35 +0900

    Update video_kivy.py

--- CamTest/UI/video_kivy.py
+++ CamTest/UI/video_kivy.py
@@ -1,3 +1,4 @@
+import os
 from tkinter import Button
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
@@ -8,8 +9,10 @@ from kivy.uix.recycleview.views import RecycleDataViewBehavior
 from kivy.properties import BooleanProperty, ListProperty
 from kivy.uix.behaviors import FocusBehavior
 from kivy.uix.recycleview.layout import LayoutSelectionBehavior
+from kivy.uix.recycleview import RecycleView
 import pymysql
 
+path = 'C:\\Users\\sjms1\\Desktop\\video'
 class RootWidget(Screen):
     def vidname(self):
         vid_list = []
@@ -22,12 +25,65 @@ class RootWidget(Screen):
 class WindowManager(ScreenManager):
     pass
 
+class Menu(Screen):
+    pass
+
+class Video_list(Screen):
+    data_items_norm = ListProperty([])
+    def get_board(self):
+        self.data_items_norm.append('App_download.mp4')
+        self.data_items_norm.append('NORM_200407_215033.mp4')
+        self.data_items_norm.append('video.mp4')
+
+class SelectableRecycleBoxLayout(FocusBehavior, LayoutSelectionBehavior,
+                                 RecycleBoxLayout):
+    ''' Adds selection and focus behaviour to the view. '''
+
+
+class SelectableButton(RecycleDataViewBehavior, Button):
+    source = ''
+    ''' Add selection support to the Label '''
+    index = None
+    selected = BooleanProperty(False)
+    selectable = BooleanProperty(True)
+
+    def refresh_view_attrs(self, rv, index, data):
+        ''' Catch and handle the view changes '''
+        self.index = index
+        return super(SelectableButton, self).refresh_view_attrs(
+            rv, index, data)
+
+    def on_touch_down(self, touch):
+        ''' Add selection on touch down '''
+        if super(SelectableButton, self).on_touch_down(touch):
+            return True
+        if self.collide_point(*touch.pos) and self.selectable:
+            return self.parent.select_with_touch(self.index, touch)
+
+    def apply_selection(self, rv, index, is_selected):
+        ''' Respond to the selection of items in the view. '''
+        self.selected = is_selected
+
+    def on_release(self, *args):
+        App.get_running_app().root.current = "root"
+
+    def on_press(self):
+        data = self.text
+        self.source = os.path.join(path, data)
+        data_index = self.index
+
+    def get_source(self):
+        return self.source
+
 class VideoPlayerApp(App):
     def build(self):
-
         sm = ScreenManager()
         self.root = RootWidget()
+        self.menu = Menu()
+        self.video_list = Video_list()
         sm.add_widget(self.root)
+        sm.add_widget(self.menu)
+        sm.add_widget(self.video_list)
         return sm
 
 ui = Builder.load_file("play.kv")
